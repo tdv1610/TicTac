ec9919c81c21a1c178d5ac07884f3e754a0e18c5
lvlykatie <tranthanh04122310@gmail.com>
2024-05-15 14:23:04 +0700

    "sua packet"

--- src/GUI/ChiTietCongViec.java
+++ src/GUI/ChiTietCongViec.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package source;
+package GUI;
 
 import java.io.File;
 import javax.swing.DefaultListModel;
