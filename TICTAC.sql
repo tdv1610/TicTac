120c7e110b2c3c7a6b710701fc4b3872d15cd763
hdngocthuy <ngocthuythanhthien@gmail.com>
2024-05-18 12:38:41 +0700

    Update database

--- TICTAC.sql
+++ TICTAC.sql
@@ -59,16 +59,16 @@ grant unlimited tablespace to c##tictac;
 REM INSERTING into AD
 SET DEFINE OFF;
 INSERT INTO AD VALUES ('22521438@gm.uit.edu.vn', 'Hu?nh Anh Th?', '1234athu');
-INSERT INTO AD VALUES ('22521455@gm.uit.edu.vn', 'Ho‡ng D??ng Ng?c Th?y', '2252thuy');
-INSERT INTO AD VALUES ('khanhphuong06@gmail.com', 'Tr?n Kh·nh Ph??ng', '062004');
-INSERT INTO AD VALUES ('quocanhle@gmail.com', 'LÍ Nguy?n Qu?cc Anh', 'anhle2222');
-INSERT INTO AD VALUES ('ngocanhnguyen@gmail.com', 'Nguy?n LÍ Ng?c Anh', 'ahihihi');
+INSERT INTO AD VALUES ('22521455@gm.uit.edu.vn', 'Ho√†ng D??ng Ng?c Th?y', '2252thuy');
+INSERT INTO AD VALUES ('khanhphuong06@gmail.com', 'Tr?n Kh√°nh Ph??ng', '062004');
+INSERT INTO AD VALUES ('quocanhle@gmail.com', 'L√™ Nguy?n Qu?cc Anh', 'anhle2222');
+INSERT INTO AD VALUES ('ngocanhnguyen@gmail.com', 'Nguy?n L√™ Ng?c Anh', 'ahihihi');
 
 REM INSERTING into NGUOIDUNG
 SET DEFINE OFF;
 INSERT INTO NGUOIDUNG VALUES ('22521366@gm.uit.edu.vn', 'Thanh Tr?n', '1366thanh');
-INSERT INTO NGUOIDUNG VALUES ('phongle@gmail.com', 'LÍ Thanh Phong', '246810phong');
-INSERT INTO NGUOIDUNG VALUES ('baonhanxyz@gmail.com', 'Nh‚n LÍ', 'abcd1234');
+INSERT INTO NGUOIDUNG VALUES ('phongle@gmail.com', 'L√™ Thanh Phong', '246810phong');
+INSERT INTO NGUOIDUNG VALUES ('baonhanxyz@gmail.com', 'Nh√¢n L√™', 'abcd1234');
 INSERT INTO NGUOIDUNG VALUES ('thuytrang004@gmail.com', 'Trang', 'tttranggg');
 
 REM INSERTING into NGUOIDUNG_NHOM
@@ -80,24 +80,24 @@ INSERT INTO NGUOIDUNG_NHOM VALUES ('thuytrang004@gmail.com','2');
 
 REM INSERTING into NHOM
 SET DEFINE OFF;
-INSERT INTO NHOM VALUES ('1', 'Ch?y ?? ·n sml', '22521366@gm.uit.edu.vn');
-INSERT INTO NHOM VALUES ('2', '‘n thi sml', 'thuytrang004@gmail.com');
+INSERT INTO NHOM VALUES ('1', 'Ch?y ?? √°n sml', '22521366@gm.uit.edu.vn');
+INSERT INTO NHOM VALUES ('2', '√în thi sml', 'thuytrang004@gmail.com');
 
 REM INSERTING into CONGVIEC
 SET DEFINE OFF;
-INSERT INTO CONGVIEC VALUES ('1', '1', 'T?o giao di?n ?? ·n', 'CNTT', 'T?o b?ng Java Swing', 1, '01-MAY-24', '15-MAY-24');
+INSERT INTO CONGVIEC VALUES ('1', '1', 'T?o giao di?n ?? √°n', 'CNTT', 'T?o b?ng Java Swing', 1, '01-MAY-24', '15-MAY-24');
 INSERT INTO CONGVIEC VALUES ('2', '1', 'T?o database', 'CNTT', 'T?o trong Oracle', 1, '10-MAY-24', '15-MAY-24');
-INSERT INTO CONGVIEC VALUES ('3', '1', 'H?c SD Github', 'CNTT', 'T? tÏm ki?m ngu?n h?c', 3, '01-MAY-24', '16-MAY-24');
-INSERT INTO CONGVIEC VALUES ('4', '2', '‘n thi CK HDH', 'CNTT', 'TÏm t‡i li?u Ùn', 2, '15-MAY-24', '16-JUN-24');
-INSERT INTO CONGVIEC VALUES ('5', '2', 'L?y g?c HQTCSDL', 'CSDL', 'TÏm b‡i gi?ng', 2, '10-MAY-24', '15-JUN-24');
+INSERT INTO CONGVIEC VALUES ('3', '1', 'H?c SD Github', 'CNTT', 'T? t√¨m ki?m ngu?n h?c', 3, '01-MAY-24', '16-MAY-24');
+INSERT INTO CONGVIEC VALUES ('4', '2', '√în thi CK HDH', 'CNTT', 'T√¨m t√†i li?u √¥n', 2, '15-MAY-24', '16-JUN-24');
+INSERT INTO CONGVIEC VALUES ('5', '2', 'L?y g?c HQTCSDL', 'CSDL', 'T√¨m b√†i gi?ng', 2, '10-MAY-24', '15-JUN-24');
 
 REM INSERTING into THUCHIEN
 SET DEFINE OFF;
-INSERT INTO THUCHIEN VALUES ('22521366@gm.uit.edu.vn', '1','1', 'C?n l‡m');
-INSERT INTO THUCHIEN VALUES ('22521366@gm.uit.edu.vn', '3','1', 'C?n l‡m');
-INSERT INTO THUCHIEN VALUES ('phongle@gmail.com', '2','1' ,'C?n l‡m');
-INSERT INTO THUCHIEN VALUES ('baonhanxyz@gmail.com', '4','2', 'C?n l‡m');
-INSERT INTO THUCHIEN VALUES ('thuytrang004@gmail.com', '5','2', '?ang l‡m');
+INSERT INTO THUCHIEN VALUES ('22521366@gm.uit.edu.vn', '1','1', 'C?n l√†m');
+INSERT INTO THUCHIEN VALUES ('22521366@gm.uit.edu.vn', '3','1', 'C?n l√†m');
+INSERT INTO THUCHIEN VALUES ('phongle@gmail.com', '2','1' ,'C?n l√†m');
+INSERT INTO THUCHIEN VALUES ('baonhanxyz@gmail.com', '4','2', 'C?n l√†m');
+INSERT INTO THUCHIEN VALUES ('thuytrang004@gmail.com', '5','2', '?ang l√†m');
 --------------------------------------------------------------------------------
 --  Constraints for Table AD
 --------------------------------------------------------
@@ -172,7 +172,229 @@ INSERT INTO THUCHIEN VALUES ('thuytrang004@gmail.com', '5','2', '?ang l‡m');
 
 ---------------------------------------------------------------------------------------------------
 
+--TRIGGER
+--CHECK_NGAYBD_CONGVIEC
+CREATE OR REPLACE TRIGGER TRIGGER_CHECK_NGAYBD
+BEFORE INSERT OR UPDATE ON CONGVIEC
+FOR EACH ROW
+BEGIN
+    IF (:NEW.NGAYBD <= CURRENT_DATE) THEN
+        raise_application_error(-20001, 'Thoi gian bat dau khong the nho hon thoi gian hien tai');
+    END IF;
+END;
+
+----------------------------------------------------------------------------------------------------
+--CHECK_NGAYKT_CONGVIEC
+CREATE OR REPLACE TRIGGER TRIGGER_CHECK_NGAYKT
+BEFORE INSERT OR UPDATE ON CONGVIEC
+FOR EACH ROW
+BEGIN
+    IF (:NEW.NGAYKT <= :NEW.NGAYBD) THEN
+        raise_application_error(-20001, 'Thoi gian ket thuc khong the nho hon hoac bang thoi gian bat dau cong viec');
+    END IF;
+END;
+
+----------------------------------------------------------------------------------------------------
 -- PROCEDURE
+-- THEM NGUOI DUNG
+CREATE OR REPLACE PROCEDURE THEM_USER (
+    I_EMAIL IN NGUOIDUNG.EMAILND%TYPE, 
+    I_TENUSER IN NGUOIDUNG.TENND%TYPE, 
+    I_MATKHAU IN NGUOIDUNG.MATKHAU%TYPE
+)
+IS
+BEGIN
+    INSERT INTO NGUOIDUNG (EMAILND, TENND, MATKHAU) 
+    VALUES (I_EMAIL, I_TENUSER, I_MATKHAU);
+    DBMS_OUTPUT.PUT_LINE('Them user thanh cong');
+EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+        DBMS_OUTPUT.PUT_LINE('Email da ton tai');
+END;
+
+-----------------------------------------------------------------------------------------------------
+-- CAP NHAT NGUOI DUNG
+create or replace PROCEDURE CAPNHAT_NGUOIDUNG (
+    I_EMAIL IN NGUOIDUNG.EMAILND%TYPE, 
+    I_TENUSER IN NGUOIDUNG.TENND%TYPE, 
+    I_MATKHAU IN NGUOIDUNG.MATKHAU%TYPE, 
+    I_MANHOM IN NGUOIDUNG_NHOM.MANHOM%TYPE
+)
+IS
+BEGIN
+    UPDATE NGUOIDUNG
+    SET TENND = I_TENUSER, MATKHAU = I_MATKHAU
+    WHERE EMAILND = I_EMAIL;
+    IF SQL%NOTFOUND THEN
+        DBMS_OUTPUT.PUT_LINE('User khong ton tai');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Cap nhat user thanh cong');
+    END IF;
+END;
+
+---------------------------------------------------------------------------------------------------------
+--XOA NGUOI DUNG
+CREATE OR REPLACE PROCEDURE XOA_NGUOIDUNG (
+    I_EMAIL IN NGUOIDUNG.EMAILND%TYPE
+)
+IS
+    L_EMAIL NGUOIDUNG.EMAILND%TYPE;
+BEGIN
+    SELECT EMAILND INTO L_EMAIL
+    FROM NGUOIDUNG
+    WHERE EMAILND = I_EMAIL;
+
+    IF L_EMAIL IS NULL THEN
+        DBMS_OUTPUT.PUT_LINE('User khong ton tai');
+    ELSE
+        -- X√≥a t·∫•t c·∫£ c√°c b·∫£n ghi li√™n quan ƒë·∫øn ng∆∞·ªùi d√πng n√†y tr∆∞·ªõc
+        XOA_THUCHIEN_THEO_EMAIL(I_EMAIL);
+        XOA_NHOM_THEO_EMAIL(I_EMAIL);
+        XOA_NHOM_THEO_EMAIL(I_EMAIL);
+
+        -- Sau ƒë√≥, ti·∫øn h√†nh x√≥a ng∆∞·ªùi d√πng
+        DELETE FROM NGUOIDUNG
+        WHERE EMAILND = I_EMAIL;
+        DBMS_OUTPUT.PUT_LINE('Xoa user thanh cong');
+    END IF;
+
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('User khong ton tai');
+
+END;
+
+-- XOA CAC BANG GHI LIEN QUAN
+-- XOA EMAIL TRONG BANG GHI LIEN QUAN
+CREATE OR REPLACE PROCEDURE XOA_NHOM_THEO_EMAIL (
+    I_EMAIL IN NGUOIDUNG.EMAILND%TYPE
+)
+IS
+BEGIN
+    -- X√≥a c√°c c√¥ng vi·ªác thu·ªôc nh√≥m
+    DELETE FROM CONGVIEC
+    WHERE MANHOM = (SELECT MANHOM FROM NGUOIDUNG_NHOM WHERE EMAILND = I_EMAIL);
+    -- X√≥a c√°c b·∫£n ghi trong b·∫£ng THUCHIEN li√™n k·∫øt v·ªõi nh√≥m
+    DELETE FROM THUCHIEN
+    WHERE MANHOM = (SELECT MANHOM FROM NGUOIDUNG_NHOM WHERE EMAILND = I_EMAIL);
+
+    DELETE FROM NHOM
+    WHERE EMAIL_TRUONGNHOM = I_EMAIL;
+
+    DELETE FROM NGUOIDUNG_NHOM
+    WHERE EMAILND = I_EMAIL;
+    DBMS_OUTPUT.PUT_LINE('Xoa tat ca cac ban ghi trong bang NHOM lien quan den nguoi dung co email ' || I_EMAIL || ' thanh cong');
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('User khong ton tai');
+END;
+
+-- XOA EMAIL TRONG BANG THUC HIEN
+CREATE OR REPLACE PROCEDURE XOA_THUCHIEN_THEO_EMAIL (
+    I_EMAIL IN NGUOIDUNG.EMAILND%TYPE
+)
+IS
+BEGIN
+    DELETE FROM THUCHIEN
+    WHERE EMAIL_TV = I_EMAIL;
+    DBMS_OUTPUT.PUT_LINE('');
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('');
+END;
+
+-------------------------------------------------------------------------------------------------------------
+-- THEM CONG VIEC
+create or replace PROCEDURE THEM_CONGVIEC (
+    I_MACV IN CONGVIEC.MACV%TYPE,
+    I_TENCV IN CONGVIEC.TENCV%TYPE,
+    I_MANHOM IN CONGVIEC.MANHOM%TYPE,
+    I_LINHVUC IN CONGVIEC.LINHVUC%TYPE,
+    I_MOTACV IN CONGVIEC.MOTACV%TYPE,
+    I_MUC_UUTIEN IN CONGVIEC.MUC_UUTIEN%TYPE,
+    I_NGAYBD IN CONGVIEC.NGAYBD%TYPE,
+    I_NGAYKT IN CONGVIEC.NGAYKT%TYPE
+)
+IS
+BEGIN
+    INSERT INTO CONGVIEC (MACV, MANHOM, TENCV, LINHVUC, MOTACV, MUC_UUTIEN, NGAYBD, NGAYKT) 
+    VALUES (I_MACV, I_MANHOM, I_TENCV, I_LINHVUC, I_MOTACV, I_MUC_UUTIEN, I_NGAYBD, I_NGAYKT);
+    DBMS_OUTPUT.PUT_LINE('Them cong viec thanh cong');
+EXCEPTION
+    WHEN DUP_VAL_ON_INDEX THEN
+        DBMS_OUTPUT.PUT_LINE('Ma cong viec da ton tai');
+END;
+
+-----------------------------------------------------------------------------------------------------------------
+--CAP NHAT CONG VIEC
+create or replace PROCEDURE CAPNHAT_CONGVIEC (
+    P_MACV IN CONGVIEC.MACV%TYPE,
+    P_TENCV IN CONGVIEC.TENCV%TYPE,
+    P_MANHOM IN CONGVIEC.MANHOM%TYPE,
+    P_LINHVUC IN CONGVIEC.LINHVUC%TYPE,
+    P_MOTACV IN CONGVIEC.MOTACV%TYPE,
+    P_MUC_UUTIEN IN CONGVIEC.MUC_UUTIEN%TYPE,
+    P_NGAYBD IN CONGVIEC.NGAYBD%TYPE,
+    P_NGAYKT IN CONGVIEC.NGAYKT%TYPE
+)
+AS
+BEGIN
+    UPDATE CONGVIEC
+    SET TENCV = P_TENCV,
+        MANHOM = P_MANHOM,
+        LINHVUC = P_LINHVUC,
+        MOTACV = P_MOTACV,
+        MUC_UUTIEN = P_MUC_UUTIEN,
+        NGAYBD = P_NGAYBD,
+        NGAYKT = P_NGAYKT
+    WHERE MACV = P_MACV;
+    IF SQL%NOTFOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Cong viec khong ton tai');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Cap nhat cong viec thanh cong');
+    END IF;
+END;
+
+------------------------------------------------------------------------------------------------------
+--CAP NHAT TRANG  THAI CONG VIEC
+create or replace PROCEDURE CAPNHAT_TRANGTHAI_CONGVIEC (
+    P_MACV_PC THUCHIEN.MACV_PC%TYPE,
+    P_TRANGTHAI THUCHIEN.TRANGTHAI%TYPE
+)
+IS 
+    L_MACV VARCHAR2(30);
+BEGIN
+    SELECT MACV_PC INTO L_MACV
+    FROM THUCHIEN
+    WHERE MACV_PC = P_MACV_PC;
+    IF L_MACV IS NULL THEN
+        DBMS_OUTPUT.PUT_LINE('Cong viec khong ton tai');
+    ELSE
+        UPDATE THUCHIEN
+        SET TRANGTHAI = P_TRANGTHAI
+        WHERE MACV_PC = P_MACV_PC;
+        DBMS_OUTPUT.PUT_LINE('Cap nhat trang thai thanh cong');
+    END IF;
+    EXCEPTION WHEN NO_DATA_FOUND THEN
+    DBMS_OUTPUT.PUT_LINE('Cong viec khong ton tai');
+END;
+
+------------------------------------------------------------------------------------------------------
+-- XEM CONG VIEC CO TRANG THAI DA LAM
+create or replace PROCEDURE XEM_CONGVIEC_DA_LAM 
+AS
+BEGIN
+    FOR rec IN (
+        SELECT CV.MACV, CV.TENCV, CV.LINHVUC
+        FROM CONGVIEC CV
+        JOIN THUCHIEN TH ON CV.MACV = TH.MACV_PC
+        WHERE TH.TRANGTHAI = 'Da lam'
+        )
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('Ma cong viec: ' || rec.MACV || ', Ten cong viec: ' || rec.TENCV || ', Linh vuc: ' || rec.LINHVUC);
+    END LOOP;
+END;
+
 -- XEM CONG VIEC CO TRANG THAI DANG LAM
 CREATE OR REPLACE PROCEDURE pc_xemcvdanglam IS
 BEGIN
@@ -180,9 +402,9 @@ BEGIN
                         FROM CONGVIEC cv
                         JOIN NHOM nh ON cv.MANHOM = nh.MANHOM
                         JOIN THUCHIEN th ON cv.MACV = th.MACV_PC AND cv.MANHOM = th.MANHOM
-                        WHERE th.TRANGTHAI = '?ang l‡m') 
+                        WHERE th.TRANGTHAI = '?ang l√†m') 
     LOOP
-        -- Xu ly in ra muc do uu tiÍn
+        -- Xu ly in ra muc do uu ti√™n
         DECLARE 
             muc_uutien_str VARCHAR2(20);
         BEGIN
@@ -195,13 +417,13 @@ BEGIN
             END IF;
             
             -- In ra thong tin cv 
-            DBMS_OUTPUT.PUT_LINE('TÍn nhÛm: ' || cv_danglam.TENNHOM);
-            DBMS_OUTPUT.PUT_LINE('TÍn cÙng vi?c: ' || cv_danglam.TENCV);
+            DBMS_OUTPUT.PUT_LINE('T√™n nh√≥m: ' || cv_danglam.TENNHOM);
+            DBMS_OUTPUT.PUT_LINE('T√™n c√¥ng vi?c: ' || cv_danglam.TENCV);
             DBMS_OUTPUT.PUT_LINE('L?nh v?c: ' || cv_danglam.LINHVUC);
-            DBMS_OUTPUT.PUT_LINE('MÙ t?: ' || cv_danglam.MOTACV);
-            DBMS_OUTPUT.PUT_LINE('Ng‡y b?t ??u: ' || TO_CHAR(cv_danglam.NGAYBD, 'DD-MON-YY'));
-            DBMS_OUTPUT.PUT_LINE('Ng‡y k?t th˙c: ' || TO_CHAR(cv_danglam.NGAYKT, 'DD-MON-YY'));
-            DBMS_OUTPUT.PUT_LINE('M?c ?? ?u tiÍn: ' || muc_uutien_str);
+            DBMS_OUTPUT.PUT_LINE('M√¥ t?: ' || cv_danglam.MOTACV);
+            DBMS_OUTPUT.PUT_LINE('Ng√†y b?t ??u: ' || TO_CHAR(cv_danglam.NGAYBD, 'DD-MON-YY'));
+            DBMS_OUTPUT.PUT_LINE('Ng√†y k?t th√∫c: ' || TO_CHAR(cv_danglam.NGAYKT, 'DD-MON-YY'));
+            DBMS_OUTPUT.PUT_LINE('M?c ?? ?u ti√™n: ' || muc_uutien_str);
             DBMS_OUTPUT.PUT_LINE('-------------------------');
         END;
     END LOOP;
@@ -218,9 +440,9 @@ BEGIN
                         FROM CONGVIEC cv
                         JOIN NHOM nh ON cv.MANHOM = nh.MANHOM
                         JOIN THUCHIEN th ON cv.MACV = th.MACV_PC AND cv.MANHOM = th.MANHOM
-                        WHERE th.TRANGTHAI = 'C?n l‡m') 
+                        WHERE th.TRANGTHAI = 'C?n l√†m') 
     LOOP
-        -- X? l˝ in ra m?c ?? ?u tiÍn
+        -- X? l√Ω in ra m?c ?? ?u ti√™n
         DECLARE muc_uutien_str VARCHAR2(20);
         BEGIN
             IF cv_selam.MUC_UUTIEN = 1 THEN
@@ -231,7 +453,7 @@ BEGIN
                 muc_uutien_str := 'Th??ng';
             END IF;
             
-            -- In ra thÙng tin cÙng viec
+            -- In ra th√¥ng tin c√¥ng viec
             DBMS_OUTPUT.PUT_LINE('Ten nhom: ' || cv_selam.TENNHOM);
             DBMS_OUTPUT.PUT_LINE('Ten cong viec: ' || cv_selam.TENCV);
             DBMS_OUTPUT.PUT_LINE('Linh vuc: ' || cv_selam.LINHVUC);
@@ -262,7 +484,7 @@ BEGIN
     JOIN THUCHIEN th ON cv.MACV = th.MACV_PC
     WHERE th.MACV_PC = p_macv_pc;
     
-    -- So s·nh ng‡y ket th˙c voi ng‡y hien tai
+    -- So s√°nh ng√†y ket th√∫c voi ng√†y hien tai
     IF v_ngaykt < SYSDATE THEN
         v_message := 'Cong viec da qua han nop';
     ELSIF v_ngaykt = TRUNC(SYSDATE) THEN
@@ -396,7 +618,7 @@ BEGIN
     INTO v_count
     FROM THUCHIEN
     WHERE MANHOM = p_manhom
-    AND TRANGTHAI = '?„ ho‡n th‡nh';
+    AND TRANGTHAI = '?√£ ho√†n th√†nh';
 
     RETURN v_count;
 EXCEPTION
@@ -421,7 +643,7 @@ BEGIN
     SELECT COUNT(*)
     INTO v_count
     FROM THUCHIEN
-    WHERE MANHOM = p_manhom AND TRANGTHAI = '?ang l‡m';
+    WHERE MANHOM = p_manhom AND TRANGTHAI = '?ang l√†m';
 
     RETURN v_count;
 EXCEPTION
@@ -445,7 +667,7 @@ BEGIN
     SELECT COUNT(*)
     INTO v_count
     FROM THUCHIEN
-    WHERE MANHOM = p_manhom AND TRANGTHAI = 'C?n l‡m';
+    WHERE MANHOM = p_manhom AND TRANGTHAI = 'C?n l√†m';
 
     RETURN v_count;
 EXCEPTION
