847d664396bff9f80981e7136bb7c41a9739ac90
ThuHuynh1438 <thu0938905081@gmail.com>
2024-05-20 15:25:02 +0700

    het loi QLTK again

--- src/DAO/CongViecDAO.java
+++ src/DAO/CongViecDAO.java
@@ -3,17 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
-import DTO.NhomDTO;
-import DTO.CongViecDTO;
-import DAO.CongViecDAO;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.sql.Types;
-import java.sql.CallableStatement; 
+
 /**
  *
  * @author VICTUS
--- src/DAO/NhomDAO.java
+++ src/DAO/NhomDAO.java
@@ -3,12 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAO;
-import DTO.NguoiDungDTO;
-import DTO.NhomDTO;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 
 /**
  *
